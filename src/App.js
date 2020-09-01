c1b3eb5434aec404eebbe803f02c8176ca1e2575
Serge Najimov <najimovserge@gmail.com>
2020-09-01 16:43:20 +0200

    basic layout

--- src/App.js
+++ src/App.js
@@ -1,24 +1,36 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="container">
+        <div className="top">
+          <div className="top__full-currency">
+            United States Dollar
+          </div>
+          <div className="top__amount">
+            200
+          </div>
+          <div className="top__symbol-currency">
+            USD
+          </div>
+        </div>
+        <div className="arrow">
+          <i class="small material-icons">arrow_upward</i>
+        </div>
+        <div className="bottom">
+          <div className="bottom__full-currency">
+            Euro
+          </div>
+          <div className="bottom__amount">
+            187
+          </div>
+          <div className="bottom__symbol-currency">
+            EUR
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
